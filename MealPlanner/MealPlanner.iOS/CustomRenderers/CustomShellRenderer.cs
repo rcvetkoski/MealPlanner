5f5cf5de6d184ba05943101cb87859ee1b8da0a1
rcvetkoski <50323773+rcvetkoski@users.noreply.github.com>
2022-10-18 18:22:04 +0200

    Create CustomShellRenderer.cs

---
+++ MealPlanner/MealPlanner.iOS/CustomRenderers/CustomShellRenderer.cs
@@ -0,0 +1,61 @@
+﻿using CoreGraphics;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using MealPlanner.iOS.CustomRenderers;
+
+[assembly: ExportRenderer(typeof(Shell), typeof(CustomShellRenderer))]
+namespace MealPlanner.iOS.CustomRenderers
+{
+    public class CustomShellRenderer : ShellRenderer
+    {
+        protected override IShellPageRendererTracker CreatePageRendererTracker()
+        {
+            return new CustomShellPageRendererTracker(this);
+        }
+    }
+
+    public class CustomShellPageRendererTracker : ShellPageRendererTracker
+    {
+        public CustomShellPageRendererTracker(IShellContext context)
+            : base(context)
+        {
+
+        }
+
+        protected override void UpdateTitleView()
+        {
+            if (ViewController == null || ViewController.NavigationItem == null)
+                return;
+
+            var titleView = Shell.GetTitleView(Page);
+
+            if (titleView == null)
+            {
+                var view = ViewController.NavigationItem.TitleView;
+                ViewController.NavigationItem.TitleView = null;
+                view?.Dispose();
+            }
+            else
+            {
+                var view = new CustomTitleViewContainer(titleView);
+                ViewController.NavigationItem.TitleView = view;
+            }
+        }
+    }
+
+    public class CustomTitleViewContainer : UIContainerView
+    {
+        public CustomTitleViewContainer(View view) : base(view)
+        {
+            TranslatesAutoresizingMaskIntoConstraints = false;
+        }
+
+        public override CGSize IntrinsicContentSize => UILayoutFittingExpandedSize;
+    }
+}
