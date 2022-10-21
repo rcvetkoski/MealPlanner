cd3fd63bdf05502d71e6dc23ef2655925e9d9f65
rcvetkoski <50323773+rcvetkoski@users.noreply.github.com>
2022-10-21 12:09:12 +0200

    ...

---
+++ MealPlanner/MealPlanner.Android/CustomRenderers/CustomShellRenderer.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MealPlanner.Droid.CustomRenderers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(Shell), typeof(CustomShellRenderer))]
+namespace MealPlanner.Droid.CustomRenderers
+{
+    public  class CustomShellRenderer : ShellRenderer
+    {
+        public CustomShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellToolbarAppearanceTracker CreateToolbarAppearanceTracker()
+        {
+            return new MyShellToolbarAppearanceTracker(this);
+        }
+    }
+
+    public class MyShellToolbarAppearanceTracker : ShellToolbarAppearanceTracker
+    {
+        public MyShellToolbarAppearanceTracker(IShellContext context) : base(context)
+        {
+        }
+
+        public override void SetAppearance(AndroidX.AppCompat.Widget.Toolbar toolbar, IShellToolbarTracker toolbarTracker, ShellAppearance appearance)
+        {
+            base.SetAppearance(toolbar, toolbarTracker, appearance);
+            //toolbar.SetPadding(0, 0, 0, 0);
+            toolbar.SetContentInsetsAbsolute(toolbar.ContentInsetStart, toolbar.ContentInsetStart);
+            //toolbar.SetContentInsetsRelative(0, 0);
+            //toolbar.NavigationIcon = null;
+        }
+    }
+}
